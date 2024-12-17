e8767a48a0e084a21061ca04b3527dc304ae2250
Mike Yang <yfractal@gmail.com>
2024-12-17 09:18:42 +0800

    feat: init otel-snitch-rb

---
+++ otel-snitch-rb/lib/otel_snitch.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "otel_snitch/version"
+
+module OtelSnitch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ otel-snitch-rb/lib/otel_snitch/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OtelSnitch
+  VERSION = "0.1.0"
+end
