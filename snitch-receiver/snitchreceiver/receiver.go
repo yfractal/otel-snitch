6284a5471616ed6ba82d6fecd890403545b0bb3d
Mike Yang <yfractal@gmail.com>
2024-12-19 09:48:58 +0800

    feat: added readfile

--- snitch-receiver/snitchreceiver/receiver.go
+++ snitch-receiver/snitchreceiver/receiver.go
@@ -2,6 +2,7 @@ package snitchreceiver
 
 import (
 	"context"
+	"net/http"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
@@ -20,13 +21,44 @@ func (receiver *snitchReceiver) Start(ctx context.Context, host component.Host) 
 	receiver.host = host
 	ctx = context.Background()
 	ctx, receiver.cancel = context.WithCancel(ctx)
+	receiver.startHTTPServer(ctx)
 
 	return nil
 }
 
-func (tailtracerRcvr *snitchReceiver) Shutdown(ctx context.Context) error {
-	if tailtracerRcvr.cancel != nil {
-		tailtracerRcvr.cancel()
+func (receiver *snitchReceiver) Shutdown(ctx context.Context) error {
+	if receiver.cancel != nil {
+		receiver.cancel()
 	}
 	return nil
+}
+
+func (receiver *snitchReceiver) startHTTPServer(ctx context.Context) {
+	http.HandleFunc("/traces", func(w http.ResponseWriter, r *http.Request) {
+		file := r.URL.Query().Get("file")
+		if file == "" {
+			http.Error(w, "Missing 'file' parameter", http.StatusBadRequest)
+			return
+		}
+
+		receiver.logger.Info("Received a request for traces.", zap.String("file", file))
+
+		ReadFile(file)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Traces received"))
+	})
+
+	server := &http.Server{Addr: ":8081"}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			receiver.logger.Fatal("HTTP server ListenAndServe", zap.Error(err))
+		}
+	}()
+
+	<-ctx.Done()
+	if err := server.Shutdown(context.Background()); err != nil {
+		receiver.logger.Fatal("HTTP server Shutdown", zap.Error(err))
+	}
 }
